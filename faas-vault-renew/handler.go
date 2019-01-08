a7cb8ea55b8e640cf0ceb06d86861d98bb6f64f0
Douglas Sanders <dsanders@onetechnologies.net>
2019-01-08 15:41:02 -0600

    proved that deps will work

--- faas-vault-renew/handler.go
+++ faas-vault-renew/handler.go
@@ -2,9 +2,15 @@ package function
 
 import (
 	"fmt"
+
+	"github.com/google/uuid"
 )
 
 // Handle a serverless request
 func Handle(req []byte) string {
-	return fmt.Sprintf("Hello, Go. You said: %s", string(req))
+	// Pull repository
+
+	deptest, _ := uuid.NewUUID()
+
+	return fmt.Sprintf("Hello, Go. You said: %s %s", string(req), deptest.String())
 }
