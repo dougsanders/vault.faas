19174a80341d2b83623d283d5dc4884a9992938a
Douglas Sanders <dsanders@onetechnologies.net>
2019-01-14 12:49:15 -0600

    added git file retrieval

--- vault-renew/handler.go
+++ vault-renew/handler.go
@@ -1,84 +1,23 @@
 package function
 
 import (
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"os"
-
-	git "gopkg.in/src-d/go-git.v4"
-	"gopkg.in/src-d/go-git.v4/plumbing/object"
-	"gopkg.in/src-d/go-git.v4/plumbing/transport/http"
 )
 
+type Request struct {
+	Environment string
+	// This can be Unseal, Access, All, a token, or many tokens
+	Filter string
+}
+
 // Handle a serverless request
-func Handle(req []byte) string {
-	usernameBytes, err := ioutil.ReadFile("/var/openfaas/secrets/username")
-	if err != nil {
-		log.Fatal(err)
-	}
-	passwordBytes, err := ioutil.ReadFile("/var/openfaas/secrets/password")
-	if err != nil {
+func Handle(bytes []byte) string {
+	req := &Request{}
+	if err := json.Unmarshal(bytes, req); err != nil {
 		log.Fatal(err)
 	}
 
-	// Pull repository
-	o := &git.CloneOptions{
-		URL: "https://github.com/OneTechLP/devops.vault.git",
-		Auth: &http.BasicAuth{
-			Username: string(usernameBytes),
-			Password: string(passwordBytes),
-		},
-		Progress: os.Stdout,
-	}
-	r, err := git.PlainClone("./", false, o)
-	if err != nil {
-		return fmt.Sprintf("{\"error\": \"%s\"}", err.Error())
-	}
-
-	// visit := func(path string, info os.FileInfo, err error) error {
-	// 	if info.IsDir() {
-	// 		fmt.Println("dir:  ", path)
-	// 	} else {
-	// 		fmt.Println("file: ", path)
-	// 	}
-	// 	return nil
-	// }
-
-	// err = filepath.Walk("./", visit)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// if _, err := os.Stat("./devops.vault"); os.IsNotExist(err) {
-	// 	return fmt.Sprintf("{\"message\": \"git repo exists\"}")
-	// }
-
-	ref, err := r.Head()
-	if err != nil {
-		return fmt.Sprintf("{\"error\": \"%s\"}", err.Error())
-	}
-
-	commit, err := r.CommitObject(ref.Hash())
-	if err != nil {
-		return fmt.Sprintf("{\"error\": \"%s\"}", err.Error())
-	}
-
-	tree, err := commit.Tree()
-	if err != nil {
-		return fmt.Sprintf("{\"error\": \"%s\"}", err.Error())
-	}
-
-	tree.Files().ForEach(func(f *object.File) error {
-		fmt.Printf("File %s    %s\n", f.Hash, f.Name)
-		return nil
-	})
-
-	dir, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(dir)
-
-	return fmt.Sprintf("Hello, Go. You said: %s", string(req))
+	return fmt.Sprintf("Hello, Go. You said: %s", string(bytes))
 }
